bbd7f15f1548ccfedbd0fcf33d266fe720d1f197
Jason J Choi <ktownexplorer2026@gmail.com>
2026-03-03 08:40:45 -0800

    increase size of country name and change to semi bold

--- src/app/[country]/CountryPage.tsx
+++ src/app/[country]/CountryPage.tsx
@@ -1130,7 +1130,7 @@ export default function CountryPage({
           className="h-8 w-auto"
         />
         <div>
-          <h1 className="text-3xl font-bold text-gray-900">{country.name}</h1>
+          <h1 className="text-4xl font-semibold text-gray-900">{country.name}</h1>
           <p className="text-sm text-gray-500">
             {country.native_name && country.native_name !== country.name && `${country.native_name} · `}{country.official_name}
           </p>
