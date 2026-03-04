98cc538e3b102bbcbc91467ea181ac13270511a8
Jason J Choi <ktownexplorer2026@gmail.com>
2026-03-03 17:22:40 -0800

    label change dial from abroad

--- src/app/[country]/CountryPage.tsx
+++ src/app/[country]/CountryPage.tsx
@@ -2160,7 +2160,7 @@ export default function CountryPage({
                 )}
                 {phoneInfo.how_to_dial_international && (
                   <div className="px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                    <dt className="text-sm font-medium text-gray-900">Dial Internationally</dt>
+                    <dt className="text-sm font-medium text-gray-900">Dial from Abroad</dt>
                     <dd className="mt-1 text-sm text-gray-700 sm:col-span-2 sm:mt-0">{phoneInfo.how_to_dial_international}</dd>
                   </div>
                 )}
